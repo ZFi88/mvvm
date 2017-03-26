793bb9dc24fdd491745c6d088ea5fc0695f9a8b8
Vildar <vildar82@gmail.com>
2017-03-26 09:20:04 +0300

    DialogCloser

---
+++ MicroMvvm/source/DialogCloser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MicroMvvm
+{
+    /// <summary>
+    /// http://stackoverflow.com/questions/501886/how-should-the-viewmodel-close-the-form
+    /// Во ViewModel добавить свойство bool? DialogResult
+    /// В View добавиьт строчки:
+    /// xmlns:xc="clr-namespace:MicroMvvm.DialogCloser;assembly=MicroMvvm"
+    /// xc:DialogCloser.DialogResult="{Binding DialogResult}">
+    /// </summary>
+    public static class DialogCloser
+    {
+        public static readonly DependencyProperty DialogResultProperty = 
+            DependencyProperty.RegisterAttached(
+                "DialogResult",
+                typeof(bool?),
+                typeof(DialogCloser),
+                new PropertyMetadata(DialogResultChanged));
+
+        private static void DialogResultChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var window = d as Window;
+            if (window != null)
+                window.DialogResult = e.NewValue as bool?;
+        }
+        public static void SetDialogResult(Window target, bool? value)
+        {
+            target.SetValue(DialogResultProperty, value);
+        }
+    }
+}
